ca6bf9a2dc56363f2551919084090722f985c0db
Alex Arcos <arcos@mps-med.de>
2019-12-10 16:19:57 +0100

    Add(cli): secret-mv command

--- vault/secret.py
+++ vault/secret.py
@@ -96,6 +96,7 @@ class Secret:
         :returns: None
 
         """
+
         for secret in self.recursive_list(engine_path, path):
             self.delete(engine_path, secret)
 
@@ -113,6 +114,67 @@ class Secret:
         response = self.vault.requests_request("GET", address, headers=self.vault.token_header)
         secret_details = response.json()["data"]["data"]
         return secret_details
+
+    def recursive_mv(self, engine_path, from_path, to_path):
+        """move the given folders with all secrets and versions
+
+        :engine_path: path of the secret engine
+        :from_path: path of the folders
+        to_path: path to move the folders
+        :returns: None
+
+        """
+        print("Original From Path:", from_path)
+        print("Original To Path:", to_path)
+        if not from_path.endswith("/"):
+            from_path = from_path + "/"
+
+        if not to_path.endswith("/"):
+            to_path = to_path + "/"
+
+        for secret in self.recursive_list(engine_path, from_path):
+            if secret.endswith("/"):
+                continue
+            new_secret_path = secret.replace(from_path, to_path)
+            self.mv(engine_path, secret, new_secret_path)
+
+    def mv(self, engine_path, from_path, to_path):
+        """ move the given secret with all its versions
+
+        :engine_path: path of the secret engine
+        :from_path: path of the secret
+        :to_path: path to move the secret to
+        :returns: None
+
+        """
+
+        versions = self._read_version(engine_path, from_path)
+        path = self.vault.normalize("/" + engine_path + "/data/" + from_path)
+        secret_details = {}
+        for version in versions:
+            address = self.vault.vault_adress + "/v1" + path + "?version={}".format(version)
+            logging.info("Reading the secret: %s", address)
+            response = self.vault.requests_request("GET", address, headers=self.vault.token_header)
+            secret_details[version] = response.json()["data"]["data"]
+            data = secret_details[version]
+            self.add(engine_path, to_path, data)
+        self.delete(engine_path, from_path)
+
+    def _read_version(self, engine_path, path):
+        """ read the versions of the given secret
+
+        :engine_path: path of the secret engine
+        :path: path of the secret
+        :returns: secret details as dict
+
+        """
+        path = self.vault.normalize("/" + engine_path + "/metadata/" + path)
+        address = self.vault.vault_adress + "/v1" + path
+        logging.info("Reading the secret: %s", address)
+        response = self.vault.requests_request("GET", address, headers=self.vault.token_header)
+        secret_details = response.json()["data"]["versions"]
+        new_list = secret_details.keys()
+        return new_list
 
 
 def add(args, vault):
@@ -133,6 +195,7 @@ def delete(args, vault):
         return
     vault.secret.delete(args.engine, args.vaultpath)
 
+
 def list_secrets(args, vault):
     """Run this module
     :returns: None
@@ -142,6 +205,7 @@ def list_secrets(args, vault):
     for secret in secret_list:
         print(secret)
 
+
 def read(args, vault):
     """Run this module
     :returns: None
@@ -149,6 +213,17 @@ def read(args, vault):
     """
     secret_details = vault.secret.read(args.engine, args.vaultpath)
     print(secret_details)
+
+
+def mv(args, vault):
+    """Run this module
+    :returns: None
+
+    """
+    if args.recursive:
+        vault.secret.recursive_mv(args.engine, args.vaultpath, args.target_vaultpath)
+        return
+    vault.secret.recursive_mv(args.engine, args.vaultpath, args.target_vaultpath)
 
 
 def parse_commandline_arguments(subparsers, config):
@@ -160,13 +235,15 @@ def parse_commandline_arguments(subparsers, config):
     del_parser = subparsers.add_parser("secret-del")
     list_parser = subparsers.add_parser("secret-list")
     read_parser = subparsers.add_parser("secret-read")
+    mv_parser = subparsers.add_parser("secret-mv")
 
     add_parser.set_defaults(func=add)
     del_parser.set_defaults(func=delete)
     list_parser.set_defaults(func=list_secrets)
     read_parser.set_defaults(func=read)
+    mv_parser.set_defaults(func=mv)
 
-    for parser in [add_parser, del_parser, list_parser, read_parser]:
+    for parser in [add_parser, del_parser, list_parser, read_parser, mv_parser]:
         if config is not None and "secret" in config and "engine" in config["secret"]:
             parser.add_argument(
                 "engine",
@@ -181,11 +258,14 @@ def parse_commandline_arguments(subparsers, config):
 
         parser.add_argument(
             "vaultpath",
-            help="path where to add the password inside the secret engine vault",
+            help="path of the secret inside the secret engine vault",
         )
+
+    mv_parser.add_argument("target_vaultpath", help="path to move the secret to")
 
     add_parser.add_argument("data", help="data of the secret as json")
 
-    del_parser.add_argument(
-        "-r", "--recursive", help="deletes secrets recursively", action="store_true"
-    )
+    for parser in [del_parser, mv_parser]:
+        parser.add_argument(
+            "-r", "--recursive", help="deletes secrets recursively", action="store_true"
+        )
